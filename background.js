7a65629370b6177efa9efde6cf29cae6a6ffa60f
Kesara Wimal <kesarawimal9@gmail.com>
2018-04-18 10:27:26 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
-chrome.browserAction.setBadgeText({text: "0"});
-//
+// chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
+// chrome.browserAction.setBadgeText({text: "0"});
+// 
 // chrome.tabs.onActivated.addListener(function() {
 //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 //         chrome.tabs.sendMessage(tabs[0].id, {greeting: "hi"}, function(response) {
@@ -15,12 +15,12 @@ chrome.browserAction.setBadgeText({text: "0"});
 //     });
 // });
 
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hi"}, function(response) {
-        chrome.browserAction.setBadgeText({text: response.farewell});
-        alert(response.farewell);
-    });
+//     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hi"}, function(response) {
+//         chrome.browserAction.setBadgeText({text: response.farewell});
+//         alert(response.farewell);
+//     });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-});
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+// });
